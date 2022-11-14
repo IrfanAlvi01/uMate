7d666c3af5656d8369db33d0899289176b2be58d
IrfanAlvi <darkshadow0067@gmail.com>
2022-11-14 23:18:48 +0500

    Created components & index-css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import {Navbar, VideoDetail, Feed, ChannelDetail, SearchFeed} from './components';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Box sx = {{background: '#0000'}}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element= {<Feed/>} />
+          <Route path='/video:id' element={<VideoDetail />}/>
+          <Route path='/channel:id' element={<ChannelDetail/>}/>
+          <Route path='/search:searchTerm' element={<SearchFeed/>}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
