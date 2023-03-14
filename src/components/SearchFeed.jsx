f8a4ceae3611775dd68b7f0a6a1138c7f3c81e48
irfanalvi67 <darkshadow0067@gmail.com>
2023-03-14 21:33:22 +0500

    started working on SearchFeed & implemented its api

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Videos } from "./";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+
+  // console.log("data", videos);
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "82.5vh", flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight={"bold"}
+        mb={2}
+        sx={{ color: "white" }}
+      >
+        Search Results for: 
+        <span style={{ color: "#F31503" }}> videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
