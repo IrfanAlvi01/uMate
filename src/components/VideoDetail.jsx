e238aea63f07415af931009fd82528c192f48571
irfanalvi67 <darkshadow0067@gmail.com>
2023-03-19 19:30:06 +0500

    Started working on video detail component & ReactPlayer

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+
+  if (!videoDetail?.snippet) return "Loading...";
+
+  const {
+    snippet: { title },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" fontWeight="bold" variant="h5" p={2}>
+              {title || ""}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
